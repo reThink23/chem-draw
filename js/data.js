846e881b15fd227b7facdbec6a6fac37b544b83e
reThink23 <42834529+reThink23@users.noreply.github.com>
2023-03-30 23:08:24 +0200

    add more settings &create css file &create data.js

---
+++ js/data.js
@@ -0,0 +1,11 @@
+const atoms = ["C", "H", "O", "P", "N", "S", "Cl", "Br", "I", "Si", "Mg", "Mn"]; // elems.keys();
+const customAtoms = [];
+
+// const settingsData = {
+// 	atom: $persist('C'), 
+// 	// location: $persist('0 0'), 
+// 	locX: $persist(0), 
+// 	locY: $persist(0),
+// 	distance: $persist(100), 
+// 	saveAlert: false
+// }
